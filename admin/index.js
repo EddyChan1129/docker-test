ebfd084c61085734ffff052c06f34ced888554c1
EddyChan1129 <22026266d@connect.polyu.hk>
2025-06-07 12:16:42 -0400

    test react

--- admin/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/admin', (req, res) => res.send('🔴 Admin Panel Active'));
-app.listen(3002, '0.0.0.0', () => console.log('Admin running on 3002'));
--- api/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/api', (req, res) => res.send('🟡 API server working!'));
-app.listen(3001, '0.0.0.0', () => console.log('API running on 3001'));
